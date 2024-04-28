2de565225008155874697ba3c30bf606c4572568
UnikMask <alexandre.guerville@hotmail.com>
2024-04-28 17:06:23 +0200

    feat(rofi): Add main rofi files to general dotfiles

---
+++ .config/rofi/iroh-powermenu.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+
+# === === === === === === === ===
+# Creator: UnikMask
+# Email: <visorunik@gmail.com>
+# === === === === === === === ===
+
+# Command for rofi
+rofi_cmd="rofi -theme iroh-powermenu.rasi"
+
+# Options
+shutdown="⏻"
+reboot=""
+lock="󰌾"
+suspendtram=""
+suspendtdisk="󰋊"
+logoutuser="󰗽"
+
+# Values given to rofi
+options="$shutdown\n$reboot\n$lock\n$suspendtram\n$suspendtdisk\n$logoutuser"
+
+fcmd="$(echo -e $options | $rofi_cmd -dmenu -selected-row 1)"
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+    case $fcmd in
+        $shutdown) 
+            poweroff -i;;
+        $reboot) 
+            reboot -i;;
+        $lock) 
+            swaylock;;
+        $suspendtram) 
+            systemctl suspend ;;
+        $suspendtdisk)
+            systemctl hibernate ;;
+        $logoutuser) 
+            swaymsg exit;;
+    esac
+else
+    case $fcmd in
+        $shutdown) 
+            poweroff -i;;
+        $reboot) 
+            reboot -i;;
+        $lock) 
+            slock;;
+        $suspendtram) 
+            systemctl suspend ;;
+        $suspendtdisk)
+            systemctl hibernate ;;
+        $logoutuser) 
+            i3-msg exit;;
+    esac
+fi
+
---
+++ .config/rofi/launchIroh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# === === === === === === === ===
+# Creator: UnikMask
+# Mail: <visorunik@gmail.com>
+# === === === === === === === ===
+
+style="iroh-drun"
+
+rofi -no-lazy-grab -show drun -theme "$style".rasi
